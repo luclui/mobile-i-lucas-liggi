190dc3908b3ada5e9bc6a4bc7fdedb4af7aeefee
LucasLiggi <lucasvechiliggi@gmail.com>
2024-03-14 08:24:35 -0300

    Aula 03 sobre métodos e funções

---
+++ aula03/funcoes1.js
@@ -0,0 +1,15 @@
+function Soma () {//cria a funcao para puxar dps
+    let n1 = 2;
+    let n2 = 6;
+
+    console.log(n1+n2);
+}
+
+Soma()//puxa o resultado da soma
+
+function SomaComParamentros (n1, n2){ // declçara para receber parametros depois
+    console.log(n1+n2);
+}
+
+SomaComParamentros(8, 10);
+SomaComParamentros(50, 10);
---
+++ aula03/metodos.js
@@ -0,0 +1,12 @@
+let txt= "Hello amigos";
+
+console.log(txt.length);//conta o numero de caracter
+
+
+console.log(txt.slice(6, 12))//pega uma parte de uma string
+
+console.log(txt.replace("Hello", "Ola"))//troca palavras de uma string
+
+let newTxt= txt.replace("Hello", "Ola")
+
+console.warn(newTxt.toUpperCase())
